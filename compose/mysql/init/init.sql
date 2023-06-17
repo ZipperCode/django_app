36b76364145cef5b47b747ecf8e1bf7799a52c96
zhangzhipeng <906043695@qq.com>
2023-06-17 21:40:23 +0800

    feat: update

--- compose/mysql/init/init.sql
+++ compose/mysql/init/init.sql
@@ -1,5 +1,5 @@
 # compose/mysql/init/init.sql
-CREATE DATABASE line_upload;
-Alter user 'line_upload'@'%' IDENTIFIED WITH mysql_native_password BY '949389';
-GRANT ALL PRIVILEGES ON line_upload.* TO 'line_upload'@'%';
+CREATE DATABASE line;
+Alter user 'line'@'%' IDENTIFIED WITH mysql_native_password BY '949389';
+GRANT ALL PRIVILEGES ON line.* TO 'line'@'%';
 FLUSH PRIVILEGES;
