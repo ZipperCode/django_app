e30cf8600e9cf54d7d678935b44fb76814bdc6bb
ZipperCode <906043695@qq.com>
2024-01-07 22:01:22 +0800

    feat: update

--- compose/uwsgi/backup_log.sh
+++ compose/uwsgi/backup_log.sh
@@ -5,6 +5,7 @@ echo "backup_suffix = $backup_suffix"
 if [ -z "$backup_suffix" ]; then
     echo "backup_suffix not found"
     exit 1
+fi
 log_file_name="web_app-uwsgi.log"
 if [ ! -f "${log_file_name}" ]; then
     echo "web_app-uwsgi.log not found"
