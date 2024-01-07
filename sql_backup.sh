a0f3438259831e4cd593bf6fc43037a8dbe72a3c
ZipperCode <906043695@qq.com>
2024-01-08 00:40:43 +0800

    feat: update

--- sql_backup.sh
+++ sql_backup.sh
@@ -10,12 +10,12 @@ echo "开始备份数据库";
 ## mysqldump -h106.14.XX.XXX -uusername -p"password123"  demand_database| gzip > /data/mysqlbackup/databaseName`date +%Y-%m-%d_%H%M%S`.sql.gz;
 
 # 最近转投docker怀抱，本地不安装mysql时，采用docker的mysql备份，备份语句修改为**
-docker exec mysql sh -c 'exec mysqldump --all-databases -utemegram -p949389 --all-databases' > /data/backup/database_`date +%Y-%m-%d_%H%M%S`.sql;
+docker exec mysql sh -c 'exec mysqldump --all-databases -utemegram -p949389 --all-databases' > /home/backup/database_`date +%Y-%m-%d_%H%M%S`.sql;
 
 # mysql 替换为对应的容器名
 # 删除 3 天前的备份文件
 
-backupdir=/data/backup
+backupdir=/home/backup
 db_name=databaseName_
 
 find $backupdir -name $db_name"*.sql.gz" -type f -mtime +3 -exec rm -rf {} \;
