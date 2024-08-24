fa0afd9cbe8113f3fd54d781c9a43996e336aa5f
ZipperCode <906043695@qq.com>
2024-08-24 16:15:19 +0800

    feat: update

--- start.sh
+++ start.sh
@@ -48,7 +48,7 @@ python3 manage.py migrate web_app
 #python3 manage.py migrate --fake web_app
 echo "exec migrate end"
 
-python3 manage.py collectstatic
+#python3 manage.py collectstatic
 
 uwsgi --ini /var/www/html/django_app/uwsgi.ini && tail -f /dev/null
 exec "$@"
